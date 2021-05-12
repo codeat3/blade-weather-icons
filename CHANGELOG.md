b985101359a904d926f5e060a1c3fe6b283739f5
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-05-12 11:35:20 +0530

    added socialcard banner

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 This changelog follows [the Keep a Changelog standard](https://keepachangelog.com).
 
+## 1.2.0 (2021-05-12)
+Added support for config file
+
+## 1.1.0 (2021-04-01)
+updated icons with latest icons and cleanup of svgs and scripts
+
 ## 1.0.0 (2021-03-30)
 * adding support for blade-icons v1.0.0
 
--- src/BladeWeatherIconsServiceProvider.php
+++ src/BladeWeatherIconsServiceProvider.php
@@ -6,25 +6,38 @@ namespace Codeat3\BladeWeatherIcons;
 
 use BladeUI\Icons\Factory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Container\Container;
 
 final class BladeWeatherIconsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->callAfterResolving(Factory::class, function (Factory $factory) {
-            $factory->add('weather-icons', [
-                'path' => __DIR__.'/../resources/svg',
-                'prefix' => 'wi',
-            ]);
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-weather-icons', []);
+
+            $factory->add('weather-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
+
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/blade-weather-icons.php', 'blade-weather-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-wi'),
-            ], 'blade-wi');
+                __DIR__.'/../resources/svg' => public_path('vendor/blade-weather-icons'),
+            ], 'blade-weather-icons');
+
+            $this->publishes([
+                __DIR__.'/../config/blade-weather-icons.php' => $this->app->configPath('blade-weather-icons.php'),
+            ], 'blade-weather-icons-config');
         }
     }
+
 }
